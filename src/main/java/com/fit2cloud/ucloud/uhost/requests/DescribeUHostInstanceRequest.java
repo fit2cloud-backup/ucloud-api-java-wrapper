517b7ffd89a469f5a14205a2586be5d432cdde91
Impasse <haifeng@fit2cloud.com>
2017-05-30 23:03:38 +0800

    fix bug

--- src/main/java/com/fit2cloud/ucloud/uhost/requests/DescribeUHostInstanceRequest.java
+++ src/main/java/com/fit2cloud/ucloud/uhost/requests/DescribeUHostInstanceRequest.java
@@ -13,10 +13,19 @@ import java.util.Map;
 public class DescribeUHostInstanceRequest extends Request{
     private String Region;
     private List<String> UHostIds;
+    private String UHostIdsStr;
     private String Tag;
     private int Offset = 0;
     private int Limit = 20;
     private Integer ProjectId;
+
+    public String getUHostIdsStr() {
+        return UHostIdsStr;
+    }
+
+    public void setUHostIdsStr(String UHostIdsStr) {
+        this.UHostIdsStr = UHostIdsStr;
+    }
 
     public String getRegion() {
         return Region;
@@ -80,22 +89,19 @@ public class DescribeUHostInstanceRequest extends Request{
 
     @Override
     public Map toMap() {
-        //当UHostIds的size大于1时用原先的方法会出现Json解析的异常因为UHostIds在Json中时数组，而new TypeToken<Map<String, String>中为String，
-        //这就导致冲突了，但是换成new TypeToken<Map<String, Object>又回引发新的Bug，正常的String值最后返回的不是字面值而是java.lang.Object
-        //所以这里对UHosIds进行处理，但是这种处理目前好像没有必要，因为目前没有出现UHostIds.size大于1的情况，因为目前的需求时要么获取所有虚拟机要么
-        //获取某个虚拟机，但是这里还是处理一下。
-        if(null != UHostIds && UHostIds.size() > 1) {
+        if(null != UHostIds) {
             StringBuilder stringBuilder = new StringBuilder();
             for(String UHostId : UHostIds) {
                 stringBuilder.append(UHostId + " ");
             }
-            UHostIds = Collections.singletonList(stringBuilder.substring(0, stringBuilder.length() - 1));
+            UHostIds = null;
+            UHostIdsStr = stringBuilder.substring(0, stringBuilder.length() - 1);
         }
         Map<String, String> map = gson.fromJson(gson.toJson(this), new TypeToken<Map<String, String>>(){}.getType());
 
-        if (UHostIds != null) {
+        if (UHostIdsStr != null) {
             int i = 1;
-            String[] uHostIds = UHostIds.get(0).split(" ");
+            String[] uHostIds = UHostIdsStr.split(" ");
             for(String uHostId : uHostIds) {
                 map.put("UHostId." + i, uHostId);
                 i++;
